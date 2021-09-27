17cc992b2863f54b5b4c5e9520bbf54d7f1a5986
Rodrigo Sanchez <rodrifs@gmail.com>
2021-09-27 08:55:00 +0200

    looking sku for units in the right table

--- server/db/models/remote/wpdn_wc_product_meta_lookup.ts
+++ server/db/models/remote/wpdn_wc_product_meta_lookup.ts
@@ -2,6 +2,8 @@ import { Model, DataTypes } from "sequelize";
 import { UnitsTotals } from "../../../constants/units";
 import logger from "../../../utils/logger";
 import { remoteDatabase } from "../../config/connection";
+import { PostMeta } from "./wpdn_postmeta";
+import { META_KEYS, Posts } from "./wpdn_posts";
 
 export class ProductMeta extends Model {
   public product_id: number;
@@ -9,61 +11,90 @@ export class ProductMeta extends Model {
   public stock_quantity: number;
 
   public static async findByProductIds(ProductIds: number[]): Promise<UnitsTotals[]> {
-    const products = await ProductMeta.findAll({ where: { sku: ProductIds } });
-    return Promise.all(
-      products.map(async (product) => {
+    const posts = await PostMeta.findAll({
+      where: { meta_key: META_KEYS.Sku, meta_value: ProductIds },
+      include: { model: Posts, where: { post_status: "publish" } },
+    });
+
+    const totals = await Promise.all(
+      posts.map(async (post) => {
+        const total = await ProductMeta.findOne({ where: { product_id: post.post_id } });
+        const lowStock = await PostMeta.findOne({ where: { post_id: post.post_id, meta_key: META_KEYS.LowStock } });
+        if (!post) {
+          return {};
+        }
+
         return {
-          productId: parseInt(product.sku),
-          quantity: product.stock_quantity,
-          lowStock: 0,
+          productId: parseInt(post?.meta_value),
+          quantity: total.stock_quantity,
+          lowStock: parseInt(lowStock?.meta_value ?? "0"),
         };
       })
     );
+
+    return totals.filter((t) => t.productId) as UnitsTotals[];
   }
 
   public static async substractFromTotals(totals: Record<string, number>): Promise<void> {
     await Promise.all(
       Object.entries(totals).map(async ([ProductId, quantity]) => {
-        const product = await ProductMeta.findOne({ where: { sku: ProductId } });
-        if (!product) {
+        const post = await PostMeta.findOne({
+          where: { meta_key: META_KEYS.Sku, meta_value: ProductId },
+          include: { model: Posts, where: { post_status: "publish" } },
+        });
+        if (!post) {
           return logger.error(
             `El sku del producto Nº${ProductId} no existe en wordpress.
              Cantidad vendida: ${quantity}. Actualizar manualmente.`
           );
         }
-        const newTotal = product.stock_quantity - quantity;
+        const total = await ProductMeta.findOne({ where: { product_id: post.post_id } });
+        if (!total) {
+          return logger.error(`
+          No hay creado un stock para el producto Nº${ProductId} en wordpress.
+          Cantidad vendida: ${quantity}. Actualizar manualmente.`);
+        }
+        const newTotal = total.stock_quantity - quantity;
         if (newTotal < 0) {
           logger.warn(`
           El total del producto Nº${ProductId} es menor a 0.
-          Cantidad en wordpress: ${product.stock_quantity},
+          Cantidad en wordpress: ${total.stock_quantity},
           Cantidad vendida: ${quantity},
           Venta extra (total - venta): ${newTotal}
           `);
-          product.stock_quantity = 0;
+          total.stock_quantity = 0;
         } else {
-          product.stock_quantity = newTotal;
+          total.stock_quantity = newTotal;
         }
-        await product.save();
+        await total.save();
       })
     );
   }
 
   public static async changeAmount(ProductId: string, action: string, amount: number) {
-    const product = await ProductMeta.findOne({ where: { sku: ProductId } });
-    if (!product) {
+    const post = await PostMeta.findOne({
+      where: { meta_key: META_KEYS.Sku, meta_value: ProductId },
+      include: { model: Posts, where: { post_status: "publish" } },
+    });
+    if (!post) {
+      logger.error(`El producto Nº${ProductId} no existe, SKU no encontrado`);
+      return { error: true };
+    }
+    const total = await ProductMeta.findOne({ where: { product_id: post.post_id } });
+    if (!total) {
       logger.error(`El producto Nº${ProductId} no existe, SKU no encontrado`);
       return { error: true };
     }
     if (action === "add") {
-      const newTotal = product.stock_quantity + amount;
-      product.stock_quantity = newTotal;
+      const newTotal = total.stock_quantity + amount;
+      total.stock_quantity = newTotal;
     } else if (action === "replace") {
-      product.stock_quantity = amount;
+      total.stock_quantity = amount;
     } else {
       logger.error("Action is not valid");
       return { error: true };
     }
-    await product.save();
+    await total.save();
   }
 }
 
